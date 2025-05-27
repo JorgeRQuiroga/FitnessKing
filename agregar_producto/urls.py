7ed84f608e3f56275b0fd2cd1d982c812570e713
Jorge <jorge.quiroga2410@gmail.com>
2025-05-27 11:37:15 -0300

    modificacion del settings.py

--- agregar_producto/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from agregar_producto import views
-
-urlpatterns = [
-    path('', views.agregar_producto, name='agregar_producto'),
-]
--- agregar_producto/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def agregar_producto(request):
-    if request.method == 'POST':
-        pass
-
-    return render(request, 'agregar_producto.html')
