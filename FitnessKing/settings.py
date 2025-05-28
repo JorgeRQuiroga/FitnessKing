befb625b7aa4d7697dfb66e38d8ffcc341b3b1cf
Jorge <jorge.quiroga2410@gmail.com>
2025-05-28 14:36:23 -0300

    estilo corregido y terminado

--- FitnessKing/settings.py
+++ FitnessKing/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fitnessking',
         'USER': 'root',
-        'PASSWORD': '0000',
+        'PASSWORD': 'QuirogaJorge2017',
         'HOST': 'localhost',
         'PORT': '3306',
     }
